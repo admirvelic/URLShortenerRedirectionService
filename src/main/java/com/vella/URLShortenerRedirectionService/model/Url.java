4d88e67aa870e920bc5afbe9a581a2f1c7d7e5c8
vella <vadmir04@gmail.com>
2023-09-24 19:49:33 +0200

    Fix Url.java

--- src/main/java/com/vella/URLShortenerRedirectionService/model/Url.java
+++ src/main/java/com/vella/URLShortenerRedirectionService/model/Url.java
@@ -1,19 +1,16 @@
 package com.vella.URLShortenerRedirectionService.model;
 
-import jakarta.persistence.*;
+
 import lombok.*;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
 @Data
 @Getter
 @Setter
 public class Url {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String realURL;
