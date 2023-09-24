c2df0dafaec8826645c7f7dc2aa3c3bb8d7ee778
vella <vadmir04@gmail.com>
2023-09-24 13:20:20 +0200

    Format CustomErrorException.java

--- src/main/java/com/vella/URLShortenerRedirectionService/exception/CustomErrorException.java
+++ src/main/java/com/vella/URLShortenerRedirectionService/exception/CustomErrorException.java
@@ -7,25 +7,25 @@ import org.springframework.http.HttpStatus;
 @Getter
 @Setter
 public class CustomErrorException extends RuntimeException {
-  private HttpStatus status;
+    private HttpStatus status;
 
-  private Object data;
+    private Object data;
 
-  public CustomErrorException(String s, Exception e) {
-    super();
-  }
+    public CustomErrorException(String s, Exception e) {
+        super();
+    }
 
-  public CustomErrorException(String message) {
-    super(message);
-  }
+    public CustomErrorException(String message) {
+        super(message);
+    }
 
-  public CustomErrorException(HttpStatus status, String message) {
-    this(message);
-    this.status = status;
-  }
+    public CustomErrorException(HttpStatus status, String message) {
+        this(message);
+        this.status = status;
+    }
 
-  public CustomErrorException(HttpStatus status, String message, Object data) {
-    this(status, message);
-    this.data = data;
-  }
+    public CustomErrorException(HttpStatus status, String message, Object data) {
+        this(status, message);
+        this.data = data;
+    }
 }
