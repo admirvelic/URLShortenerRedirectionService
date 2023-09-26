bfc84c9dfcace5d1f9d49ce24510e994b324b4b3
vella <vadmir04@gmail.com>
2023-09-26 14:59:21 +0200

    Format UrlRepo.java

--- src/main/java/com/vella/URLShortenerRedirectionService/repository/UrlRepo.java
+++ src/main/java/com/vella/URLShortenerRedirectionService/repository/UrlRepo.java
@@ -4,6 +4,7 @@ import com.vella.URLShortenerRedirectionService.model.Url;
 import com.vella.URLShortenerRedirectionService.exception.CustomErrorException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
