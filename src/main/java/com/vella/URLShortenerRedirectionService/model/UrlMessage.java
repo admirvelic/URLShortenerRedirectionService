50422176a67b9716a954192a63c14579c52f8589
vella <vadmir04@gmail.com>
2023-09-27 22:06:56 +0200

    Format UrlMessage.java

--- src/main/java/com/vella/URLShortenerRedirectionService/model/UrlMessage.java
+++ src/main/java/com/vella/URLShortenerRedirectionService/model/UrlMessage.java
@@ -17,12 +17,5 @@ public class UrlMessage {
     private String shortURL;
 
     private Action action;
-
-    public UrlMessage(Url url, Action action) {
-        this.shortURL = url.getShortURL();
-        this.realURL = url.getRealURL();
-        this.action = action;
-    }
-
 }
 
